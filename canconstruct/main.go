38f3c8c80d349321c586c619775edb33d311e74b
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2022-03-01 22:55:42 +0700

    finish can construct

--- canconstruct/main.go
+++ canconstruct/main.go
@@ -1,7 +1,59 @@
 package main
 
-func canConstruct() {
+import (
+	"fmt"
+	"strings"
+)
+
+func canConstruct(target string, words []string) bool {
+
+	if len(target) == 0 {
+		return true
+	}
+
+	for _, v := range words {
+		if strings.HasPrefix(target, v) {
+			suffix := target[len(v):]
+			if canConstruct(suffix, words) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func canConstructMemo(target string, words []string, memo map[string]bool) bool {
+
+	if v, ok := memo[target]; ok {
+		return v
+	}
+
+	if len(target) == 0 {
+		return true
+	}
+
+	for _, v := range words {
+		if strings.HasPrefix(target, v) {
+			suffix := target[len(v):]
+			if canConstructMemo(suffix, words, memo) {
+				memo[target] = true
+				return true
+			}
+		}
+	}
+
+	memo[target] = false
+	return false
 }
 
 func main() {
+	fmt.Println(canConstruct("abcdef", []string{"ab", "abc", "cd", "def", "abcd"}))                  // true
+	fmt.Println(canConstruct("skateboard", []string{"bo", "rd", "ate", "t", "ska", "sk", "boar"}))   // false
+	fmt.Println(canConstruct("enterapotentpot", []string{"a", "p", "ent", "enter", "ot", "o", "t"})) // true
+
+	//fmt.Println(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", []string{"e", "ee", "eeee", "eeeee", "eeeeee"})) // false
+
+	memo := make(map[string]bool)
+	fmt.Println(canConstructMemo("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", []string{"e", "ee", "eeee", "eeeee", "eeeeee"}, memo)) // false
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 I just follow  freecodecamp video tutorial about dynamic programming and covert it to golang.
 
-VDO: [https://www.youtube.com/watch?v=oBt53YbR9Kk] (https://www.youtube.com/watch?v=oBt53YbR9Kk)
+VDO: [https://www.youtube.com/watch?v=oBt53YbR9Kk](https://www.youtube.com/watch?v=oBt53YbR9Kk)
